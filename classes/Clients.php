ff0db723fba9a5090d55c2a4d9f9176e12f21700
alisson10000 <alisson_lima_souza@yahoo.com.br>
2023-09-01 00:53:15 -0300

    Update Clients.php

--- classes/Clients.php
+++ classes/Clients.php
@@ -2,18 +2,11 @@
 
 class Clients extends Conexao {
 
-    
+    public function saveClient($company_id, $first_name, $last_name, $email, $phone, $mobile, $address, $zipcode, $userpic, $city, $hashed_password, $inactive, $access, $last_active, $last_login, $token, $language, $signature) {
 
-    public function saveClient( $company_id, $first_name, $last_name, $email, $phone, $mobile, $address, $zipcode, $userpic, $city, $hashed_password, $inactive, $access, $last_active, $last_login, $token, $language, $signature) {
+        $consulta = "INSERT INTO `clients`(`id`, `company_id`, `first_name`, `last_name`, `email`, `phone`, `mobile`, `address`, `zipcode`, `userpic`, `city`, `hashed_password`, `inactive`, `access`, `last_active`, `last_login`, `token`, `language`, `signature`) VALUES (NULL,'$company_id','$first_name','$last_name','$email','$phone','$mobile','$address','$zipcode','$userpic','$city','$hashed_password','$inactive','$access','$last_active','$last_login','$token','$language','$signature')";
 
-       $consulta = "INSERT INTO `clients`(`id`, `company_id`, `first_name`, `last_name`, `email`, `phone`, `mobile`, `address`, `zipcode`, `userpic`, `city`, `hashed_password`, `inactive`, `access`, `last_active`, `last_login`, `token`, `language`, `signature`) VALUES (NULL,'$company_id','$first_name','$last_name','$email','$phone','$mobile','$address','$zipcode','$userpic','$city','$hashed_password','$inactive','$access','$last_active','$last_login','$token','$language','$signature')";
-      
-    //   $consulta = "INSERT INTO `clients` (`id`, `company_id`, `first_name`, `last_name`, `email`, `phone`, `mobile`, `address`, `zipcode`, `userpic`, `city`, `hashed_password`, `inactive`, `access`, `last_active`, `last_login`, `token`, `language`, `signature`) VALUES (NULL, '$company_id', '$first_name', NULL, NULL, NULL, NULL, NULL, NULL, 'no-pic.png', NULL, NULL, 'N', '0', NULL, NULL, NULL, NULL, NULL)";
-        
-        
         $this->salvaOcorrencia($consulta);
     }
-    
-
 
 }
