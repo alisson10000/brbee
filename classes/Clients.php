a4f2c5703e5e32aa0f407b9b8fbc3ca24ba14b98
alisson10000 <alisson_lima_souza@yahoo.com.br>
2023-09-02 00:20:18 -0300

    métodos classe clients
    implementação de todos os métodos

--- classes/Clients.php
+++ classes/Clients.php
@@ -3,10 +3,24 @@
 class Clients extends Conexao {
 
     public function saveClient($company_id, $first_name, $last_name, $email, $phone, $mobile, $address, $zipcode, $userpic, $city, $hashed_password, $inactive, $access, $last_active, $last_login, $token, $language, $signature) {
-
         $consulta = "INSERT INTO `clients`(`id`, `company_id`, `first_name`, `last_name`, `email`, `phone`, `mobile`, `address`, `zipcode`, `userpic`, `city`, `hashed_password`, `inactive`, `access`, `last_active`, `last_login`, `token`, `language`, `signature`) VALUES (NULL,'$company_id','$first_name','$last_name','$email','$phone','$mobile','$address','$zipcode','$userpic','$city','$hashed_password','$inactive','$access','$last_active','$last_login','$token','$language','$signature')";
-
         $this->salvaOcorrencia($consulta);
+    }
+
+    public function updateClient($id, $company_id, $first_name, $last_name, $email, $phone, $mobile, $address, $zipcode, $userpic, $city, $hashed_password, $inactive, $access, $last_active, $last_login, $token, $language, $signature) {
+        $consulta = "UPDATE `clients` SET `company_id`='$company_id',`first_name`='$first_name',`last_name`='$last_name',`email`='$email',`phone`='$phone',`mobile`='$mobile',`address`='$address',`zipcode`='$zipcode',`userpic`='$userpic',`city`='$city',`hashed_password`='$hashed_password',`inactive`='$inactive',`access`='$access',`last_active`='$last_active',`last_login`='$last_login',`token`='$token',`language`='$language',`signature`='$signature' WHERE id='$id'";
+        $this->salvaOcorrencia($consulta);
+    }
+
+    public function deleteClient($id) {
+        $consulta = "DELETE FROM `clients` WHERE id='$id'";
+        $this->salvaOcorrencia($consulta);
+    }
+
+    public function query() {
+        $consulta = "SELECT * FROM `clients` where id > 0";
+        $atributos = ["id", "first_name"];
+        return $this->listarEntidade($consulta, $atributos);
     }
 
 }
--- index.php
+++ index.php
@@ -16,17 +16,17 @@
         $client = new Clients($_local, $_usuario, $_senha, $_banco);
         $client->conectar();
         $client->verificaConexao();
-      
+
         /*
          * Essas variáveis receberão valores de um formulário
          * 
          * 
          */
-        
-        
+
+        $id = "38";
         $company_id = "6546";
-        $first_name = "NULL";
-        $last_name = "NULL";
+        $first_name = "meliza";
+        $last_name = "sarto";
         $email = "NULL";
         $phone = "NULL";
         $mobile = "NULL";
@@ -42,13 +42,25 @@
         $token = "NULL";
         $language = "NULL";
         $signature = "NULL";
-        
-        
-        
-       $client->saveClient( $company_id, $first_name, $last_name, $email, $phone, $mobile, $address, $zipcode, $userpic, $city, $hashed_password, $inactive, $access, $last_active, $last_login, $token, $language, $signature);
-        
-        
-                
-                ?>
+
+//        $client->saveClient($company_id, $first_name, $last_name, $email, $phone, $mobile, $address, $zipcode, $userpic, $city, $hashed_password, $inactive, $access, $last_active, $last_login, $token, $language, $signature);
+//
+//        $client->updateClient($id, $company_id, $first_name, $last_name, $email, $phone, $mobile, $address, $zipcode, $userpic, $city, $hashed_password, $inactive, $access, $last_active, $last_login, $token, $language, $signature);
+//       
+        // $client->deleteClient($id);
+        ?>
+
+        <table>
+
+        <?php
+        $list = $client->query();
+
+        foreach ($list as $value) {
+
+            echo "<tr><td>" . $value['id'] . "</td><td>" . $value['first_name'] . "</td></tr>";
+        }
+        ?> 
+
+        </table>
     </body>
 </html>
